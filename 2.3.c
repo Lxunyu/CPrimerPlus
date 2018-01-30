af9ec99953a2bd24d61775b2645e74e4d455ad31
Lxunyu <35958851+Lxunyu@users.noreply.github.com>
2018-01-30 19:55:39 +0800

    2.3和3.3

---
+++ 2.3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+	int age=20,days;
+	days = age * 365;
+	printf("your age is %d,and your days is %d", age,days);
+	return 0;
+}
---
+++ 3.3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Starled by the sudden ,Sally shouted");
+	printf("By the Great Pump,what was that!");
+		return 0;
+}
