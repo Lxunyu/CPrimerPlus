c99b5528483fa765d4b308864a532d5b88f5f344
Lxunyu <35958851+Lxunyu@users.noreply.github.com>
2018-02-01 00:21:33 +0800

    3.6修改

--- 3.6.c
+++ 3.6.c
@@ -3,8 +3,8 @@ int main(void)
 {
 	double k=950, m = 3e-23,M,n;
 	printf("请输入水的夸脱数");
-	scanf("%f", &n);
-	M = n*m*k;
-	printf("水分子的数量为%d", &M);
-		return 0;
+	scanf("%Lf", &n);
+	M = n*k/m;
+	printf("水分子的数量为%f", M);
+	return 0;
 }
