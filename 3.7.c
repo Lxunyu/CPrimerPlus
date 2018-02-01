0069b180d8ddff9b848f0e269df4ea2d78605a68
Lxunyu <35958851+Lxunyu@users.noreply.github.com>
2018-02-01 22:52:58 +0800

    3.7修改

--- 3.7.c
+++ 3.7.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-	double tall, feet;
+	double tall, feet=2.54,n;
 	printf("请输入你的身高");
-		scanf("%lf", feet);
-		tall = feet*2.54;
-		printf("你的身高为%lf", tall);
+		scanf("%Lf", &n);
+		tall = feet*n;
+		printf("你的身高为%Lf", tall);
 		return 0;
 }
