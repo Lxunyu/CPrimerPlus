7517817a6806c7d403f252f03ead1582956a87e0
Lxunyu <35958851+Lxunyu@users.noreply.github.com>
2018-02-21 01:29:15 +0800

    6.5

---
+++ 6.5.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+int main(void)
+{
+	printf("请输入字母");
+	char a;
+	scanf("%c", &a);
+	int i, j;
+	for (i = 0; i < a-'A'+1; i++)
+	{
+		for (j = 0; j < a-'A' - i; j++)
+		{
+			printf(" ");
+		}
+		for (j = 0; j <= i; j++)
+			printf("%c", 'A' + j);
+		for (j = i - 1; j >= 0; j--)
+			printf("%c",'A' + j);
+		printf("\n");
+	}
+		return 0;
+}
